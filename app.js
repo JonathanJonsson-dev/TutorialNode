f5aa96ea0d2793ebbf58d42e061b70a390064b7b
Jonathan Jonsson <jonsson.jonathan@hotmail.com>
2021-12-27 22:54:32 +0100

    Async programming

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+const {readFile} = require('fs')
 
-const _ = require('lodash')
+const getText = (path)  => {
+    return new Promise((resolve, reject)=>{
+        readFile(path, 'utf-8', (err, data)=>{
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve(data)
+            }
+        })
+    })
+}
 
-const items = [1, [2, [3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems); 
-console.log("Hello World");
+//getText('./content/first.txt')
+//.then(result => console.log(result))
+//.catch(err => console.log(err))
+
+const start = async() =>{
+    try {
+        const first = await getText('./content/first.txt')
+        console.log(first);
+    } catch (error) {
+        console.log(error);    
+    }   
+}
+
+start()
