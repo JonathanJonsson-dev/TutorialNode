757ee1c30b7a5f440f3a10b4efac938fa89d7bbe
Jonathan Jonsson <jonsson.jonathan@hotmail.com>
2021-12-27 15:10:01 +0100

    First upload

---
+++ 8-os-module.js
@@ -0,0 +1,15 @@
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+// method returns the system uptime in seconds
+const uptime = os.uptime()
+console.log(`The system uptime is ${uptime / 3600} hours`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOS);
